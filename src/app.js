bfa8598d2c89f201e443b24cdbac20adaf913c5e
snik109 <jonas.flydahl@gmail.com>
2026-05-26 20:40:35 +0200

    a test to see if this solution works

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import path from 'path';
 
-dotenv.config();
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 import lookupRoutes from "./v1.0.0/routes/lookupRoutes.js";
 import rolleRoutes from "./v1.0.0/routes/rolleRoutes.js";
